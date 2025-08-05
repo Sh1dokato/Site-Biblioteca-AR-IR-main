3ab05ffe7da2b97eb44ddc1dea9cc7a55db645b6
ruan_m_vieira <ruan_m_vieira@estudante.sesisenai.org.br>
2025-08-05 16:38:16 -0300

    Add files via upload

---
+++ Site-Biblioteca-AR-IR-main-Novo/Biblioteca Site P/PHP/cadastrofornecedor.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Validação Fornecedor</title>
+</head>
+<body>
+<?php
+    if(isset($_GET['nome'])&& isset($_GET['cep'])&& isset($_GET['numCasa'])&& isset($_GET['cpfCnpj'])&& isset($_GET['telefone'])){
+        echo"Recebido fornecedor ".$_GET['nome'];
+        echo " morador do CEP ".$_GET['cep'];
+        echo " com a casa numero ".$_GET['numCasa'];
+        echo " detentor do CPF/CNPJ ".$_GET['cpfCnpj'];
+        echo " Sendo seu numero de telefone: ".$_GET['telefone'];
+    }
+?>
+</body>
+</html>
